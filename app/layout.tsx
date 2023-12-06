532e8565c615e8145830e80b8b3a81015917a814
ttt <ttt@gmail.com>
2023-12-06 16:45:24 +0800

    xxx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
+import '@/app/globals.css';
+import React from "react";
+import { inter,lusitana } from '@/app/ui/fonts';
+function get() {
+  return new Promise((resolve)=>setTimeout(resolve,3000))
+}
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  await get();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} ${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter,Roboto } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Roboto({weight: ["100","300","500","700","900"], subsets: ['latin'] });
